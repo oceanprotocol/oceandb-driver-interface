566868f274ac8f3dbab327085571548ad95d9fea
eruizgar91 <enrique@oceanprotocol.com>
2018-10-01 17:34:43 +0200

    Prepare upgrade version

--- oceandb_driver_interface/__init__.py
+++ oceandb_driver_interface/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for oceandb-plugin-system."""
 
 __author__ = """OceanProtocol"""
-__version__ = '0.1.7'
+__version__ = '0.1.9'
 
 from oceandb_driver_interface.oceandb import (
     OceanDb
