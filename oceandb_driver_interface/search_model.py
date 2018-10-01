6000f45158afeefdb1d7d49e13712b815ec1214d
eruizgar91 <enrique@oceanprotocol.com>
2018-10-01 16:52:42 +0200

    Change sorting type

--- oceandb_driver_interface/search_model.py
+++ oceandb_driver_interface/search_model.py
@@ -1,10 +1,8 @@
 from typing import Tuple, List
 
-sort = Tuple[str, str]
-
 
 class QueryModel(object):
-    def __init__(self, query=None, sorting: List[sort] = None, offset=100, page=0):
+    def __init__(self, query=None, sorting: List[Tuple] = None, offset=100, page=0):
         self.query = query
         self.sort = sorting
         self.offset = offset
@@ -12,7 +10,7 @@ class QueryModel(object):
 
 
 class FullTextModel():
-    def __init__(self, text=None, sorting: List[sort] = None, offset=100, page=0):
+    def __init__(self, text=None, sorting: List[Tuple] = None, offset=100, page=0):
         self.text = text
         self.sort = sorting
         self.offset = offset
