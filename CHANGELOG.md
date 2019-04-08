1d584c0927783212c137aac05864c3952cc516d7
eruizgar91 <enrique@oceanprotocol.com>
2019-04-08 15:58:16 +0200

    Udpate changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 History
 =======
 
+0.2.0 (April 8th, 2018)
+-----------------------
+* Update initial page in search to 1.
+
 0.1.11 (October 4th, 2018)
 --------------------------
 * Refactored config to use convention
--- oceandb_driver_interface/__init__.py
+++ oceandb_driver_interface/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for oceandb-plugin-system."""
 
 __author__ = """OceanProtocol"""
-__version__ = '0.1.11'
+__version__ = '0.2.0'
 
 #  Copyright 2018 Ocean Protocol Foundation
 #  SPDX-License-Identifier: Apache-2.0
