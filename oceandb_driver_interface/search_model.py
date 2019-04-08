fdc1a5a24e2c5e86b3e147b430c49b9a498bab20
ssallam <samer@oceanprotocol.com>
2019-04-08 14:37:49 +0200

    Validate page value.

--- oceandb_driver_interface/search_model.py
+++ oceandb_driver_interface/search_model.py
@@ -4,6 +4,7 @@
 
 class QueryModel(object):
     def __init__(self, query=None, sort: dict = None, offset=100, page=1):
+        assert page >= 1, f'Invalid page value {page}. Required page >= 1.'
         self.query = query
         self.sort = sort
         self.offset = offset
@@ -12,6 +13,7 @@ class QueryModel(object):
 
 class FullTextModel(object):
     def __init__(self, text=None, sort: dict = None, offset=100, page=1):
+        assert page >= 1, f'Invalid page value {page}. Required page >= 1.'
         self.text = text
         self.sort = sort
         self.offset = offset
